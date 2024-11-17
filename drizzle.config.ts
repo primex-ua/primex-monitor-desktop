2abeb99fa38338665d90f861d5e13f4aed2266bb
primex-ua <dmytro.martynov@outlook.com>
2024-11-17 02:29:51 +0100

    fix: Prefix db url with 'file:'

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,12 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
-import path from 'path';
-
-const DB_PATH = path.resolve(__dirname, process.env.DB_PATH!);
 
 export default defineConfig({
   out: './src/drizzle/migrations',
   schema: './src/drizzle/schema.ts',
   dialect: 'sqlite',
   dbCredentials: {
-    url: DB_PATH,
+    url: `file:${process.env.DB_PATH!}`,
   },
+  verbose: true,
 });
--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,12 +1,9 @@
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
+import { drizzle } from 'drizzle-orm/libsql';
 import * as schema from './schema';
-import path from 'path';
 
-const DB_PATH = path.resolve(__dirname, process.env.DB_PATH!);
-
-const client = createClient({ url: DB_PATH });
+const client = createClient({ url: `file:${process.env.DB_PATH!}` });
 const db = drizzle(client, { schema });
 
 export default db;
