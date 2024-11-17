97464b4448472ce6115b674129e00e2451c0feb9
primex-ua <dmytro.martynov@outlook.com>
2024-11-17 01:20:57 +0100

    refactor: Use path.resolve

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,14 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
+import path from 'path';
+
+const DB_PATH = path.resolve(__dirname, process.env.DB_PATH!);
 
 export default defineConfig({
   out: './src/drizzle/migrations',
   schema: './src/drizzle/schema.ts',
   dialect: 'sqlite',
   dbCredentials: {
-    url: process.env.DB_PATH!,
+    url: DB_PATH,
   },
 });
--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -2,8 +2,11 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import * as schema from './schema';
+import path from 'path';
 
-const client = createClient({ url: process.env.DB_PATH! });
+const DB_PATH = path.resolve(__dirname, process.env.DB_PATH!);
+
+const client = createClient({ url: DB_PATH });
 const db = drizzle(client, { schema });
 
 export default db;
