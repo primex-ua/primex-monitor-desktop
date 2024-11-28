31ed9954b73c596fd5e686b728451edc3a1bde67
primex-ua <dmytro.martynov@outlook.com>
2024-11-28 11:31:42 +0100

    Fix/schema (#4)
    * fix: Update db schema
    
    * fix: Update transformData
    
    * fix: Update apiSchema
    
    * refactor: Fix typo in db schema
    
    * feat: Add backup record option to seed function
    
    * fix: Correct schema typo
    
    * fix: Round totalWeight in seed
    
    * feat: Use realistic components masses
    
    * fix: Mode can be null
    
    * fix: Calculate total weight with respect to mixer volume

--- src/helpers/seed.ts
+++ src/helpers/seed.ts
@@ -2,28 +2,51 @@ import db from '../drizzle/db';
 import { products } from '../drizzle/schema';
 import { InsertDbRecord } from '../schemas/dbSchema';
 
+const NAMES = ['Рецепт 1', 'Рецепт 2', 'Рецепт 3', 'Рецепт 4', 'Рецепт 5', 'Без обліку'];
+const MIXER_VOULUME = 5; // m^3
+
 function getRandomNumber(min: number, max: number, decimalPart = 0) {
   const value = Math.random() * (max - min + 1) + min;
   return !decimalPart ? Math.floor(value) : parseFloat(value.toFixed(decimalPart));
 }
 
 function generateRandomRecord(): InsertDbRecord {
-  return {
+  const index = Math.floor(Math.random() * 6);
+
+  const isBackupRecord = index === 5;
+
+  const name = NAMES[index];
+
+  const components = [
+    MIXER_VOULUME * getRandomNumber(60, 100, 1), // вода
+    MIXER_VOULUME * getRandomNumber(150, 200, 1), // відсів
+    MIXER_VOULUME * getRandomNumber(350, 450, 1), // пісок
+    MIXER_VOULUME * getRandomNumber(150, 200, 1), // щебень
+    MIXER_VOULUME * getRandomNumber(150, 200, 1), // щебень 2
+    MIXER_VOULUME * getRandomNumber(150, 200, 1), // цемент
+    MIXER_VOULUME * getRandomNumber(2, 15, 2), // хім. добавки
+  ];
+
+  const totalWeight = parseFloat(components.reduce((acc, value) => acc + value, 0).toFixed(1));
+
+  const newRecord = {
     dateTime: new Date(),
-    mode: getRandomNumber(1, 2) % 2 === 0 ? 'А' : 'Р',
-    mixture: getRandomNumber(50, 1000),
-    moistureContent: getRandomNumber(1, 20, 2),
-    name: `Рецепт ${getRandomNumber(1, 30)}`,
-    Pres: getRandomNumber(1, 10),
-    water: getRandomNumber(30, 600),
-    weight: getRandomNumber(30, 600),
-    component1: getRandomNumber(30, 600, 1),
-    component2: getRandomNumber(30, 600, 1),
-    component3: getRandomNumber(30, 600, 1),
-    component4: getRandomNumber(30, 600, 1),
-    component5: getRandomNumber(30, 600, 1),
-    component6: getRandomNumber(30, 600, 1),
+    name,
+    mode: !isBackupRecord ? ((getRandomNumber(1, 2) % 2 === 0 ? 'А' : 'Р') as 'А' | 'Р') : null,
+    moistureContent: !isBackupRecord ? getRandomNumber(1, 20, 2) : null,
+    press: !isBackupRecord ? getRandomNumber(1, 10) : null,
+    specificWeight: !isBackupRecord ? getRandomNumber(500, 2500) : null,
+    water: components[0],
+    component1: components[1],
+    component2: components[2],
+    component3: components[3],
+    component4: components[4],
+    component5: components[5],
+    component6: components[6],
+    totalWeight,
   };
+
+  return newRecord;
 }
 
 async function seed(length: number = 1) {
--- src/lib/transformData.ts
+++ src/lib/transformData.ts
@@ -16,17 +16,18 @@ export function transformData(data: DbRecord[], dbSettings: DbSettings) {
     if (dbSettings.NameInComp5 && d.component5)
       materialsConsumed.push({ name: dbSettings.NameInComp5, value: d.component5 });
     if (dbSettings.NameInComp6 && d.component6)
-      materialsConsumed.push({ name: dbSettings.NameInComp6, value: d.component6 });
+      materialsConsumed.push({ name: dbSettings.NameInComp6, value: d.component6, decimalPart: 2 });
 
-    materialsConsumed.push({ name: 'Вода', value: d.water });
+    materialsConsumed.push({ name: 'Вода', value: d.water, units: 'л' });
 
     return {
       name: d.name,
       mixedAt: d.dateTime,
-      press: d.Pres,
-      mode: (d.mode === 'А' ? 'auto' : 'manual') as ApiEntry['mode'],
+      press: d.press,
+      mode: d.mode ? ((d.mode === 'А' ? 'auto' : 'manual') as ApiEntry['mode']) : null,
+      totalWeight: d.totalWeight,
       moistureContent: d.moistureContent,
-      producedRunningMeters: d.mixture, // Це має якось вираховуватись
+      producedRunningMeters: d.specificWeight !== null ? d.totalWeight / d.specificWeight : null,
       materialsConsumed,
     };
   });
